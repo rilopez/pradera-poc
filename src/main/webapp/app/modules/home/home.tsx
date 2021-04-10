6a0a615e93745c53e317a9673b05a9c97c13fe62
Isaac Lopez <ilopez@nearsoft.com>
2021-04-09 23:50:00 -0500

    pluging working, need to save

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -1,37 +1,95 @@
 import './home.scss';
 
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { Translate } from 'react-jhipster';
-import { connect } from 'react-redux';
-import { Row, Col, Alert } from 'reactstrap';
-import { useEditorState, BangleEditor } from '@bangle.dev/react';
-import { superscript, subscript } from '@bangle.dev/text-formatting';
+import React, {useEffect, useState} from 'react';
+import {Link} from 'react-router-dom';
+import {Translate} from 'react-jhipster';
+import {connect} from 'react-redux';
+import {Row, Col, Alert} from 'reactstrap';
+import {useEditorState, BangleEditor} from '@bangle.dev/react';
+import {Plugin} from '@bangle.dev/core';
+import {superscript, subscript} from '@bangle.dev/text-formatting';
+import {corePlugins, coreSpec} from '@bangle.dev/core/utils/core-components';
+import {
+  floatingMenu,
+  FloatingMenu,
+  Menu,
+  MenuGroup,
+  BoldButton,
+  HeadingButton,
+} from '@bangle.dev/react-menu';
+import {PluginKey} from '@bangle.dev/core';
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
+import {solarizedlight} from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 import '@bangle.dev/core/style.css';
+import '@bangle.dev/tooltip/style.css';
+import '@bangle.dev/react-menu/style.css';
 
 export type IHomeProp = StateProps;
 
-export const Home = (props: IHomeProp) => {
-  const { account } = props;
+const menuKey = new PluginKey('menuKey');
 
+export const Home = (props: IHomeProp) => {
+  const {account} = props;
+  const [doc, setDoc] = useState(null)
+
+  const updatePlugin = new Plugin({
+    view: () => ({
+
+      update(view, prevState) {
+        if (!view.state.doc.eq(prevState.doc)) {
+          //TODO send state to database
+          setDoc(view.state.doc.toJSON());
+        }
+      },
+    })
+  })
+
+  const editorState = useEditorState({
+    specs: coreSpec(),
+    plugins: () => [
+      ...corePlugins(),
+      updatePlugin,
+      floatingMenu.plugins({
+        key: menuKey,
+      }),
+    ],
+    initialValue: 'Hello world!',
+  });
+  //console.log("editorState.pmState.doc", editorState.pmState.doc.toJSON())
   return (
     <Row>
       <Col md="3" className="pad"></Col>
       <Col md="9">
-        <Editor></Editor>
+        <BangleEditor state={editorState}>
+          <FloatingMenu menuKey={menuKey}
+                        renderMenuType={({type}) => {
+                          if (type === 'defaultMenu') {
+                            return (
+                              <Menu>
+                                {/*<MenuGroup>*/}
+                                {/*  <BoldButton/>*/}
+                                {/*</MenuGroup>*/}
+                                <MenuGroup>
+                                  <HeadingButton level={1}/>
+                                  <HeadingButton level={2}/>
+                                </MenuGroup>
+                              </Menu>
+                            );
+                          }
+
+                          return null;
+                        }}
+          />
+          <SyntaxHighlighter language="json" style={solarizedlight} showLineNumbers={true}>
+            {JSON.stringify(doc, null, 1)}
+          </SyntaxHighlighter>
+        </BangleEditor>
       </Col>
     </Row>
   );
 };
 
-function Editor() {
-  const editorState = useEditorState({
-    specs: [],
-    initialValue: 'Hello world!',
-  });
-  return <BangleEditor state={editorState} />;
-}
 
 const mapStateToProps = storeState => ({
   account: storeState.authentication.account,

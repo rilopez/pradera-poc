205a39c0aab83ff85405beba0d030e04da4bb68e
gserrano <gserrano@nearsoft.com>
2021-06-04 19:55:32 -0500

    Converting main application to kotlin

--- src/main/java/com/pradera/poc/PraderaApp.kt
+++ src/main/java/com/pradera/poc/PraderaApp.kt
@@ -1,103 +1,103 @@
-package com.pradera.poc;
+package com.pradera.poc
 
-import com.pradera.poc.config.ApplicationProperties;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Optional;
-import javax.annotation.PostConstruct;
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.core.env.Environment;
-import tech.jhipster.config.DefaultProfileUtil;
-import tech.jhipster.config.JHipsterConstants;
+import com.pradera.poc.config.ApplicationProperties
+import org.apache.commons.lang3.StringUtils
+import org.slf4j.LoggerFactory
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.core.env.Environment
+import tech.jhipster.config.DefaultProfileUtil
+import tech.jhipster.config.JHipsterConstants
+import java.net.InetAddress
+import java.net.UnknownHostException
+import java.util.*
+import javax.annotation.PostConstruct
+import kotlin.jvm.JvmStatic
 
 @SpringBootApplication
-@EnableConfigurationProperties({ LiquibaseProperties.class, ApplicationProperties.class })
-public class PraderaApp {
-
-    private static final Logger log = LoggerFactory.getLogger(PraderaApp.class);
-
-    private final Environment env;
-
-    public PraderaApp(Environment env) {
-        this.env = env;
-    }
-
+@EnableConfigurationProperties(LiquibaseProperties::class, ApplicationProperties::class)
+class PraderaApp(private val env: Environment) {
     /**
      * Initializes pradera.
-     * <p>
+     *
+     *
      * Spring profiles can be configured with a program argument --spring.profiles.active=your-active-profile
-     * <p>
-     * You can find more information on how profiles work with JHipster on <a href="https://www.jhipster.tech/profiles/">https://www.jhipster.tech/profiles/</a>.
+     *
+     *
+     * You can find more information on how profiles work with JHipster
+     * on [https://www.jhipster.tech/profiles/](https://www.jhipster.tech/profiles/).
      */
     @PostConstruct
-    public void initApplication() {
-        Collection<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
-        if (
-            activeProfiles.contains(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT) &&
+    fun initApplication() {
+        val activeProfiles: Collection<String> = listOf(*env.activeProfiles)
+        if (activeProfiles.contains(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT) &&
             activeProfiles.contains(JHipsterConstants.SPRING_PROFILE_PRODUCTION)
         ) {
             log.error(
-                "You have misconfigured your application! It should not run " + "with both the 'dev' and 'prod' profiles at the same time."
-            );
+                "You have misconfigured your application! It should not run " +
+                    "with both the 'dev' and 'prod' profiles at the same time."
+            )
         }
-        if (
-            activeProfiles.contains(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT) &&
+        if (activeProfiles.contains(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT) &&
             activeProfiles.contains(JHipsterConstants.SPRING_PROFILE_CLOUD)
         ) {
             log.error(
-                "You have misconfigured your application! It should not " + "run with both the 'dev' and 'cloud' profiles at the same time."
-            );
+                "You have misconfigured your application! It should not " +
+                    "run with both the 'dev' and 'cloud' profiles at the same time."
+            )
         }
     }
 
-    /**
-     * Main method, used to run the application.
-     *
-     * @param args the command line arguments.
-     */
-    public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(PraderaApp.class);
-        DefaultProfileUtil.addDefaultProfile(app);
-        Environment env = app.run(args).getEnvironment();
-        logApplicationStartup(env);
-    }
+    companion object {
+        private val log = LoggerFactory.getLogger(PraderaApp::class.java)
 
-    private static void logApplicationStartup(Environment env) {
-        String protocol = Optional.ofNullable(env.getProperty("server.ssl.key-store")).map(key -> "https").orElse("http");
-        String serverPort = env.getProperty("server.port");
-        String contextPath = Optional
-            .ofNullable(env.getProperty("server.servlet.context-path"))
-            .filter(StringUtils::isNotBlank)
-            .orElse("/");
-        String hostAddress = "localhost";
-        try {
-            hostAddress = InetAddress.getLocalHost().getHostAddress();
-        } catch (UnknownHostException e) {
-            log.warn("The host name could not be determined, using `localhost` as fallback");
+        /**
+         * Main method, used to run the application.
+         *
+         * @param args the command line arguments.
+         */
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val app = SpringApplication(PraderaApp::class.java)
+            DefaultProfileUtil.addDefaultProfile(app)
+            val env: Environment = app.run(*args).environment
+            logApplicationStartup(env)
         }
-        log.info(
-            "\n----------------------------------------------------------\n\t" +
-            "Application '{}' is running! Access URLs:\n\t" +
-            "Local: \t\t{}://localhost:{}{}\n\t" +
-            "External: \t{}://{}:{}{}\n\t" +
-            "Profile(s): \t{}\n----------------------------------------------------------",
-            env.getProperty("spring.application.name"),
-            protocol,
-            serverPort,
-            contextPath,
-            protocol,
-            hostAddress,
-            serverPort,
-            contextPath,
-            env.getActiveProfiles()
-        );
+
+        private fun logApplicationStartup(env: Environment) {
+            val protocol = Optional.ofNullable(env.getProperty("server.ssl.key-store")).map { "https" }
+                .orElse("http")
+            val serverPort = env.getProperty("server.port")
+            val contextPath = Optional
+                .ofNullable(env.getProperty("server.servlet.context-path"))
+                .filter { cs: String? -> StringUtils.isNotBlank(cs) }
+                .orElse("/")
+            var hostAddress: String? = "localhost"
+            try {
+                hostAddress = InetAddress.getLocalHost().hostAddress
+            } catch (e: UnknownHostException) {
+                log.warn("The host name could not be determined, using `localhost` as fallback")
+            }
+            log.info(
+                """
+----------------------------------------------------------
+	Application '{}' is running! Access URLs:
+	Local: 		{}://localhost:{}{}
+	External: 	{}://{}:{}{}
+	Profile(s): 	{}
+----------------------------------------------------------""",
+                env.getProperty("spring.application.name"),
+                protocol,
+                serverPort,
+                contextPath,
+                protocol,
+                hostAddress,
+                serverPort,
+                contextPath,
+                env.activeProfiles
+            )
+        }
     }
 }
